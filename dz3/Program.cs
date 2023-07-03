5d736126eebd158aa85fdb7e42c524535bac3072
Miramir Babaev <miramir.babaev@yandex.ru>
2023-07-03 14:02:56 +0300

    dz3

---
+++ dz3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 3
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);              // Подсказка
+    return Convert.ToInt32(Console.ReadLine()); // Преобразование введенной пользователем строки в число
+}
+int N = ReadInt("Введите число => ");
+int count = 1;
+while (count <= N)
+{
+    System.Console.WriteLine(count*count*count);
+    count++;
+}
